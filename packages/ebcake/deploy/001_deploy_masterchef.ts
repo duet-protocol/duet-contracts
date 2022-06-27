c53c70e872f703e41b043cd4664f433506279060
gordomium <x@gordomium.me>
2022-06-27 20:10:47 +0800

    feat(ebcake): Advanced deploy guard for proxied contracts

--- packages/ebcake/deploy/001_deploy_masterchef.ts
+++ packages/ebcake/deploy/001_deploy_masterchef.ts
@@ -3,7 +3,7 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types';
 // eslint-disable-next-line node/no-unpublished-import
 import { useLogger } from '../scripts/utils';
 import { HardhatDeployRuntimeEnvironment } from '../types/hardhat-deploy';
-import { writeExtraMeta } from './.defines';
+import { advancedDeploy } from './.defines'
 
 export enum MasterChefDeployNames {
   MultiRewardsMasterChef = 'MultiRewardsMasterChef',
@@ -11,26 +11,33 @@ export enum MasterChefDeployNames {
 
 const logger = useLogger(__filename);
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+
   const { deployments, getNamedAccounts } = hre as unknown as HardhatDeployRuntimeEnvironment;
   const { deploy } = deployments;
 
   const { deployer } = await getNamedAccounts();
 
-  const masterChef = await deploy(MasterChefDeployNames.MultiRewardsMasterChef, {
-    from: deployer,
-    proxy: {
-      execute: {
-        init: {
-          methodName: 'initialize',
-          args: [deployer],
+  await advancedDeploy({
+    hre,
+    logger,
+    name: MasterChefDeployNames.MultiRewardsMasterChef,
+    proxied: true,
+  }, async ({ name }) => {
+
+    return await deploy(name, {
+      from: deployer,
+      proxy: {
+        execute: {
+          init: {
+            methodName: 'initialize',
+            args: [deployer],
+          },
         },
       },
-    },
-    log: true,
-    autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
-  });
+      log: true,
+      autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
+    });
+  })
 
-  await writeExtraMeta(MasterChefDeployNames.MultiRewardsMasterChef)
-  logger.info('MultiRewardsMasterChef deployed, ', masterChef.address);
 };
 export default func;
