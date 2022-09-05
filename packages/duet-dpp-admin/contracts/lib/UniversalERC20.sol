58ec4f34c405cbab0dc9b2feb1afdb53ba965cd6
SocratesLu <SocratesLu9631@gmail.com>
2022-09-05 19:47:22 +0800

    feat(dpp-admin):update import ffrom oz

--- packages/duet-dpp-admin/contracts/lib/UniversalERC20.sol
+++ packages/duet-dpp-admin/contracts/lib/UniversalERC20.sol
@@ -1,8 +1,8 @@
 pragma solidity 0.8.9;
 
-import { SafeMath } from "./SafeMath.sol";
-import { IERC20 } from "../interfaces/IERC20.sol";
-import { SafeERC20 } from "./SafeERC20.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { SafeMath } from "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 library UniversalERC20 {
     using SafeMath for uint256;
