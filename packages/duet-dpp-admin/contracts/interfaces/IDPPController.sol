315c1237fbd047cc76e6c6509dffbd70390aa30a
SocratesLu <SocratesLu9631@gmail.com>
2022-09-20 12:59:12 +0800

    feat(dpp-admin):add lpOracle

--- packages/duet-dpp-admin/contracts/interfaces/IDPPController.sol
+++ packages/duet-dpp-admin/contracts/interfaces/IDPPController.sol
@@ -8,4 +8,12 @@ interface IDPPController {
         address dppAdminAddress,
         address weth
     ) external;
+
+    function _BASE_TOKEN_() external view returns (address base);
+    function _QUOTE_TOKEN_() external view returns (address quote);
+
+    function recommendBaseAndQuote(uint256 shareAmount_)
+        external
+        view
+        returns (uint256 baseAmount, uint256 quoteAmount);
 }
