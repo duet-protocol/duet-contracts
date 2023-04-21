87b4dade4893f01ff34c9ae89d754db33870427b
davidduet <mr5@duet.finance>
2023-04-20 23:59:22 -0700

    feat(duet-pro): initial commit

---
+++ packages/duet-pro/config.ts
@@ -0,0 +1,10 @@
+export default {
+  address: {
+    DeriLens: {
+      arbitrum: '0x74F8acC86D93052557752E9D0B0c7b89b53ef100',
+    },
+    DeriPool: {
+      arbitrum: '0xde57c591de8b3675c43fb955725b62e742b1c0b4',
+    },
+  },
+} as const
---
+++ packages/duet-pro/contracts/interfaces/IBoosterOracle.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+interface IBoosterOracle {
+    // Must 8 dec, same as chainlink decimals.
+    function getPrice(address token) external view returns (uint256);
+}
